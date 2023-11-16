ba0c8b448af9fd48ba59c3fb453a417ae780dfd5
whanod <whanod@gmail.com>
2023-11-16 12:31:25 +0330

    add wallet connect

--- dapp/src/App.tsx
+++ dapp/src/App.tsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { FC, useMemo } from "react";
+import { WalletProvider } from "@demox-labs/aleo-wallet-adapter-react";
+import { WalletModalProvider, WalletMultiButton } from "@demox-labs/aleo-wallet-adapter-reactui";
+import { LeoWalletAdapter } from "@demox-labs/aleo-wallet-adapter-leo";
+import {
+  DecryptPermission,
+  WalletAdapterNetwork,
+} from "@demox-labs/aleo-wallet-adapter-base";
+import "@demox-labs/aleo-wallet-adapter-reactui/styles.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+// Default styles that can be overridden by your app
+
+
+export const App: FC = () => {
+  const wallets = useMemo(
+    () => [
+      new LeoWalletAdapter({
+        appName: "Leo Demo App",
+      }),
+    ],
+    []
+  );
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+    <WalletProvider
+      wallets={wallets}
+      decryptPermission={DecryptPermission.UponRequest}
+      network={WalletAdapterNetwork.Testnet}
+      autoConnect
+    >
+      <WalletModalProvider>
+    <WalletMultiButton/>
+      </WalletModalProvider>
+    </WalletProvider>
+  );
+};
--- dapp/src/main.tsx
+++ dapp/src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import {App} from './App.tsx'
 import './index.css'
+import "@demox-labs/aleo-wallet-adapter-reactui/styles.css"
+import React from 'react'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+
+
+
+
